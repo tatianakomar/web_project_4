6e342afc86fcbb93182ed99a99854ccce8f2c782
Tanya Komar <tatiana23komar@gmail.com>
2020-12-08 22:01:41 -0800

    refactoring index.js

--- script/index.js
+++ script/index.js
@@ -54,30 +54,34 @@ const deleteCard = (evt) => {
     const cardItem = evt.target.closest(".card");
     cardItem.remove();
 };
-const togglePopup = (modal) => {
-    modal.classList.toggle("popup_open");
-};
+
+const openPopup = (modal) => {
+    modal.classList.add("popup_open");
+    document.addEventListener("keydown", escapeButton);
+    document.addEventListener("click", mouseClick);
+}
+const closePopup = (modal) => {
+    const openedPopup = document.querySelector(".popup_open");
+    openedPopup.classList.remove("popup_open");
+    document.removeEventListener("keydown", escapeButton);
+    document.removeEventListener("click", mouseClick);
+}
 const closeButtons = document.querySelectorAll(".popup__close");
 closeButtons.forEach((closeButton) => {
-    const popup = closeButton.closest(".popup");
     closeButton.addEventListener("click", () => {
-
-        togglePopup(popup);
+        closePopup();
     });
 });
 
 const escapeButton = (evt) => {
     if (evt.key === "Escape") {
-        editProfilePopup.classList.remove("popup_open");
-        addNewCardPopup.classList.remove("popup_open");
-        cardPopup.classList.remove("popup_open");
+        closePopup();
     }
 }
-document.addEventListener("keydown", escapeButton);
 
-const mouseClick = (evt, popup) => {
-    if (!evt.target.closest(".popup__content")) {
-        popup.classList.remove("popup_open");
+const mouseClick = (evt) => {
+    if (!evt.target.closest(".popup__content") && !evt.target.closest(".popup__content_content_card")) {
+        closePopup();
     }
 }
 
@@ -104,10 +108,7 @@ const createCard = (link, title) => {
         popupCardImage.src = link;
         popupCardImage.alt = title;
         popupCardTitle.textContent = title;
-        document.addEventListener("click", (evt) => {
-            mouseClick(evt, cardPopup)
-        }, )
-        togglePopup(cardPopup);
+        openPopup(cardPopup);
     });
     return newCard;
 }
@@ -129,18 +130,14 @@ addNewCardButton.addEventListener("click", (evt) => {
     })
     const formSubmitButton = cardForm.querySelector(validationSettings.submitButtonSelector);
     setDisabledButton(formSubmitButton, true);
-    togglePopup(addNewCardPopup);
-    document.addEventListener("click", (evt) => {
-        mouseClick(evt, addNewCardPopup)
-    })
+    openPopup(addNewCardPopup);
 });
 // Create New Card
 cardForm.addEventListener("submit", (evt) => {
     evt.preventDefault();
     const card = createCard(cardLinkInput.value, cardTitleInput.value);
     gallery.prepend(card);
-
-    togglePopup(addNewCardPopup);
+    closePopup();
 });
 //************************************************************************
 //Open Edit Profile Popup
@@ -155,10 +152,7 @@ editProfileButton.addEventListener("click", (evt) => {
     })
     const formSubmitButton = profileForm.querySelector(validationSettings.submitButtonSelector);
     setDisabledButton(formSubmitButton, false);
-    togglePopup(editProfilePopup);
-    document.addEventListener("click", (evt) => {
-        mouseClick(evt, editProfilePopup)
-    })
+    openPopup(editProfilePopup);
 });
 //Save New Profile
 profileForm.addEventListener("submit", (evt) => {
@@ -166,5 +160,5 @@ profileForm.addEventListener("submit", (evt) => {
     // Update profile
     nameProfile.textContent = profileNameInput.value;
     jobProfile.textContent = profileJobInput.value;
-    togglePopup(editProfilePopup);
+    closePopup();
 });
